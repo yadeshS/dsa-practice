521bdf0737c62791161972063c8464e60df685ba
YADESHHARIHAR <105919962+yadeshS@users.noreply.github.com>
2026-01-07 23:53:22 -0800

    Day 3: Linked List basics in Python

---
+++ DSA_Python/Linked_list/main.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, "->", end=" ")
+            temp = temp.next
+        print("None")
+
+
+# Testing
+ll = LinkedList()
+ll.insert_at_end(10)
+ll.insert_at_end(20)
+ll.insert_at_beginning(5)
+ll.display()
