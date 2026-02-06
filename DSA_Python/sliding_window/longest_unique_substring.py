468d5169a7216624f878544bc00e37bc4ea022c1
YADESHHARIHAR <105919962+yadeshS@users.noreply.github.com>
2026-02-05 23:17:49 -0700

    Day8: variable size problems

---
+++ DSA_Python/sliding_window/longest_unique_substring.py
@@ -0,0 +1,19 @@
+def longest_unique_substring(s):
+    char_set = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+
+        char_set.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+
+# Test
+print(longest_unique_substring("abcabcbb"))  # 3
+print(longest_unique_substring("bbbbb"))     # 1
